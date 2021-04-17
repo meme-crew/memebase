e98002de03a345d3838127c2553dd0da14fcbc09
ahhh <ahhh.db@gmail.com>
2021-04-17 02:47:07 -0700

    more frontend tweaks, new to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import logo from "./images/harambe.jpg";
 function App() {
   return (
     <div className="flex flex-col h-full items-center justify-center text-white bg-gradient-to-br from-gray-600 to-gray-800">
-      <img src={logo} />
+      <div className="flex justify-center img py-2">
+        <img src={logo} />
+      </div>
       <div className="flex items-center animate-bounce">
         <p className="mt-6 tracking-wide">memebase</p>
       </div>
